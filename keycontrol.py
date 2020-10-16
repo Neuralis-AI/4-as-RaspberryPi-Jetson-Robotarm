93635c7aab5980334202bc707e9a450b3ea7171b
Neuralis-AI <64447278+Neuralis-AI@users.noreply.github.com>
2020-10-16 21:31:25 +0200

    Update keycontrol.py

--- keycontrol.py
+++ keycontrol.py
@@ -37,6 +37,7 @@ pwm.set_servo_pulsewidth( linksrPIN, 500 ) ; # Zet links/rechts op positie 0 (he
 pwm.set_servo_pulsewidth( vooraPIN, 500 ) ; # Zet voor/achter op positie 0 (helemaal vooruit)
 
 # In volgende variabelen worden de posities van elke servo bijgehouden
+# Deze positionering zit logisch in elkaar: 500 is helemaal links, 1500 is het midden en 2500 is rechts 
 grijper = 500
 bovenonder = 500
 linksrechts = 500
@@ -49,17 +50,17 @@ try: # Probeer
         pwm.set_servo_pulsewidth( linksrPIN, linksrechts ) ; # Zet links/rechts op inhoud van variabele
         pwm.set_servo_pulsewidth( vooraPIN, voorachter ) ; # Zet voor/achter op inhoud van variabele
 
-        char = getch()
+        char = getch() # Met deze functie kijken we welke toets ingedrukt is
 
-        if (char == "a"):
-            if (linksrechts < 2500):
-                linksrechts += 500
-                pwm.set_servo_pulsewidth( linksrPIN, linksrechts ) ; # Zet linksrechts op inhoud van variabele "linksrechts"
+        if (char == "a"): # Als "A" ingedrukt is
+            if (linksrechts < 2500): # En wanneer links/rechts lager is dan 2500 (het maximum)
+                linksrechts += 500 # Tel 500 op bij de vorige waarde van links/rechts (1 stap)
+                pwm.set_servo_pulsewidth( linksrPIN, linksrechts ) ; # Zet linksrechts op de nieuwe inhoud (+ 1 stap) en beweeg de arm hier naartoe
 
-        if (char == "d"):
-            if (linksrechts > 500):
-                linksrechts -= 500
-                pwm.set_servo_pulsewidth( linksrPIN, linksrechts ) ; # Zet linksrechts op inhoud van variabele "linksrechts"
+        if (char == "d"): # Als "d" ingedrukt is
+            if (linksrechts > 500): # En wanneer links/rechts hoger is dan 500 (het minimum)
+                linksrechts -= 500 # trek 500 af van de vorige waarde van links/rechts (1 stap)
+                pwm.set_servo_pulsewidth( linksrPIN, linksrechts ) ; # Zet linksrechts op de nieuwe inhoud (- 1 stap) en beweeg de arm hier naartoe
 
         if (char == "q"):
             if (grijper < 2500):
@@ -93,7 +94,8 @@ try: # Probeer
 
         else:
             if (char == "p"):
-                exit()
+                GPIO.cleanup() # Schakel alle GPIO pins uit
+                exit() # stop het script
 
 except KeyboardInterrupt: # Bij het stoppen van het script (met CTRL+C)
     GPIO.cleanup() # Herstel GPIO's naar standaardwaarden
